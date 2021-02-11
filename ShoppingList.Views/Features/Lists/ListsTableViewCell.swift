bca4ccbebbb2bddad7a20e4cde7d36234b83eb8c
Adrian Świątek <adrian@aswiatek.pl>
2021-02-11 23:42:27 +0100

    Added new element to the list cell.

--- ShoppingList.Views/Features/Lists/ListsTableViewCell.swift
+++ ShoppingList.Views/Features/Lists/ListsTableViewCell.swift
@@ -10,6 +10,7 @@ public final class ListsTableViewCell: UITableViewCell {
             accessTypeImageView.image = (viewModel.isPrivate ? #imageLiteral(resourceName: "Locked") : #imageLiteral(resourceName: "Shared")).withRenderingMode(.alwaysTemplate)
             accessTypeLabel.text = viewModel.accessType
             numberOfItemsValueLabel.text = String(viewModel.numberOfItemsToBuy)
+            hasItemsInBaskekValueLabel.text = viewModel.hasItemsInBasket ? "Yes" : "No"
             updateDateValueLabel.text = viewModel.formattedUpdateDate
         }
     }
@@ -20,21 +21,35 @@ public final class ListsTableViewCell: UITableViewCell {
         $0.font = .boldSystemFont(ofSize: 18)
         $0.numberOfLines = 0
     }
-    
+
     private let numberOfItemsLabel: UILabel = configure(.init()) {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.text = "Items to buy:"
         $0.textColor = .textSecondary
         $0.font = .systemFont(ofSize: 14)
     }
-    
+
     private let numberOfItemsValueLabel: UILabel = configure(.init()) {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.text = "0"
         $0.textColor = .textPrimary
         $0.font = .systemFont(ofSize: 14, weight: .semibold)
     }
-    
+
+    private let hasItemsInBasketLabel: UILabel = configure(.init()) {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "Has items in basket:"
+        $0.textColor = .textSecondary
+        $0.font = .systemFont(ofSize: 14)
+    }
+
+    private let hasItemsInBaskekValueLabel: UILabel = configure(.init()) {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "No"
+        $0.textColor = .textPrimary
+        $0.font = .systemFont(ofSize: 14, weight: .semibold)
+    }
+
     private let updateDateLabel: UILabel = configure(.init()) {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.text = "Updated:"
@@ -54,16 +69,16 @@ public final class ListsTableViewCell: UITableViewCell {
         $0.font = .systemFont(ofSize: 14)
         $0.textAlignment = .right
     }
-    
+
     private let accessTypeImageView: UIImageView = configure(.init()) {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.tintColor = .textSecondary
     }
-    
+
     private let accessTypeView: UIView = configure(.init()) {
         $0.translatesAutoresizingMaskIntoConstraints = false
     }
-    
+
     public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.accessoryType = .disclosureIndicator
@@ -74,7 +89,7 @@ public final class ListsTableViewCell: UITableViewCell {
     public required init?(coder aDecoder: NSCoder) {
         fatalError("Not supported.")
     }
-    
+
     private func setupView() {
         backgroundColor = .background
 
@@ -113,23 +128,35 @@ public final class ListsTableViewCell: UITableViewCell {
             numberOfItemsLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 4)
         ])
 
-        contentView.addSubview(numberOfItemsValueLabel)
+        contentView.addSubview(hasItemsInBasketLabel)
         NSLayoutConstraint.activate([
-            numberOfItemsValueLabel.leadingAnchor.constraint(equalTo: numberOfItemsLabel.trailingAnchor, constant: 4),
-            numberOfItemsValueLabel.centerYAnchor.constraint(equalTo: numberOfItemsLabel.centerYAnchor)
+            hasItemsInBasketLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            hasItemsInBasketLabel.topAnchor.constraint(equalTo: numberOfItemsLabel.bottomAnchor, constant: 4)
         ])
 
         contentView.addSubview(updateDateLabel)
         NSLayoutConstraint.activate([
             updateDateLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
-            updateDateLabel.topAnchor.constraint(equalTo: numberOfItemsLabel.bottomAnchor),
+            updateDateLabel.topAnchor.constraint(equalTo: hasItemsInBasketLabel.bottomAnchor, constant: 4),
             updateDateLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12),
-            updateDateLabel.widthAnchor.constraint(equalTo: numberOfItemsLabel.widthAnchor)
+            updateDateLabel.widthAnchor.constraint(equalTo: hasItemsInBasketLabel.widthAnchor)
+        ])
+
+        contentView.addSubview(numberOfItemsValueLabel)
+        NSLayoutConstraint.activate([
+            numberOfItemsValueLabel.leadingAnchor.constraint(equalTo: hasItemsInBasketLabel.trailingAnchor, constant: 8),
+            numberOfItemsValueLabel.centerYAnchor.constraint(equalTo: numberOfItemsLabel.centerYAnchor)
+        ])
+
+        contentView.addSubview(hasItemsInBaskekValueLabel)
+        NSLayoutConstraint.activate([
+            hasItemsInBaskekValueLabel.leadingAnchor.constraint(equalTo: hasItemsInBasketLabel.trailingAnchor, constant: 8),
+            hasItemsInBaskekValueLabel.centerYAnchor.constraint(equalTo: hasItemsInBasketLabel.centerYAnchor)
         ])
 
         contentView.addSubview(updateDateValueLabel)
         NSLayoutConstraint.activate([
-            updateDateValueLabel.leadingAnchor.constraint(equalTo: updateDateLabel.trailingAnchor, constant: 4),
+            updateDateValueLabel.leadingAnchor.constraint(equalTo: hasItemsInBasketLabel.trailingAnchor, constant: 8),
             updateDateValueLabel.centerYAnchor.constraint(equalTo: updateDateLabel.centerYAnchor)
         ])
     }
