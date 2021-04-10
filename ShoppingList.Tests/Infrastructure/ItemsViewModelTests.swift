ae13b94ba35ea7f38888c6ad1cb3eca4f4e68214
Adrian Świątek <adrian@aswiatek.pl>
2021-04-10 21:33:01 +0200

    Added ItemsViewModelTests.

--- ShoppingList.Tests/Infrastructure/ItemsViewModelTests.swift
+++ ShoppingList.Tests/Infrastructure/ItemsViewModelTests.swift
@@ -65,4 +65,122 @@ final class ItemsViewModelTests: XCTestCase {
         wait(for: [expectation], timeout: 0.1)
         XCTAssertTrue(publishedSections.flatMap { $0.items }.contains { $0.name == "Test item" })
     }
+
+    func test_addToBasketItems_publishes_sections() {
+        let item1 = itemWithName("Test name 1")
+        let item2 = itemWithName("Test name 2")
+        itemRepository.addItems([item1, item2])
+        sut.fetchItems()
+
+        let expectation = self.expectation(description: "sectionsPublisher expectation")
+        expectation.expectedFulfillmentCount = 2
+        var publishedSections = [ItemsSectionViewModel]()
+
+        sut.sectionsPublisher
+            .dropFirst()
+            .sink {
+                publishedSections = $0
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+
+        sut.addToBasketItems(with: [item1.id.toUuid()])
+
+        wait(for: [expectation], timeout: 0.1)
+
+        let publishedItems = publishedSections.flatMap { $0.items }
+        XCTAssertTrue(publishedItems.contains { $0.uuid == item2.id.toUuid() })
+        XCTAssertFalse(publishedItems.contains { $0.uuid == item1.id.toUuid() })
+    }
+
+    func test_addToBasketAllItems_publishes_sections() {
+        let item1 = itemWithName("Test name 1")
+        let item2 = itemWithName("Test name 2")
+        itemRepository.addItems([item1, item2])
+        sut.fetchItems()
+
+        let expectation = self.expectation(description: "sectionsPublisher expectation")
+        expectation.expectedFulfillmentCount = 2
+        var publishedSections = [ItemsSectionViewModel]()
+
+        sut.sectionsPublisher
+            .dropFirst()
+            .sink {
+                publishedSections = $0
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+
+        sut.addToBasketAllItems()
+
+        wait(for: [expectation], timeout: 0.1)
+
+        let publishedItems = publishedSections.flatMap { $0.items }
+        XCTAssertFalse(publishedItems.contains { $0.uuid == item2.id.toUuid() })
+        XCTAssertFalse(publishedItems.contains { $0.uuid == item1.id.toUuid() })
+    }
+
+    func test_removeItems_publishes_sections() {
+        let item1 = itemWithName("Test name 1")
+        let item2 = itemWithName("Test name 2")
+        itemRepository.addItems([item1, item2])
+        sut.fetchItems()
+
+        let expectation = self.expectation(description: "sectionsPublisher expectation")
+        expectation.expectedFulfillmentCount = 2
+        var publishedSections = [ItemsSectionViewModel]()
+
+        sut.sectionsPublisher
+            .dropFirst()
+            .sink {
+                publishedSections = $0
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+
+        sut.removeItems(with: [item1.id.toUuid()])
+
+        wait(for: [expectation], timeout: 0.1)
+
+        let publishedItems = publishedSections.flatMap { $0.items }
+        XCTAssertTrue(publishedItems.contains { $0.uuid == item2.id.toUuid() })
+        XCTAssertFalse(publishedItems.contains { $0.uuid == item1.id.toUuid() })
+    }
+
+    func test_removeAllItems_publishes_sections() {
+        let item1 = itemWithName("Test name 1")
+        let item2 = itemWithName("Test name 2")
+        itemRepository.addItems([item1, item2])
+        sut.fetchItems()
+
+        let expectation = self.expectation(description: "sectionsPublisher expectation")
+        expectation.expectedFulfillmentCount = 2
+        var publishedSections = [ItemsSectionViewModel]()
+
+        sut.sectionsPublisher
+            .dropFirst()
+            .sink {
+                publishedSections = $0
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+
+        sut.removeAllItems()
+
+        wait(for: [expectation], timeout: 0.1)
+        let publishedItems = publishedSections.flatMap { $0.items }
+        XCTAssertFalse(publishedItems.contains { $0.uuid == item2.id.toUuid() })
+        XCTAssertFalse(publishedItems.contains { $0.uuid == item1.id.toUuid() })
+    }
+
+    private func itemWithName(_ name: String) -> Item {
+        Item(
+            id: .random(),
+            name: name,
+            info: nil,
+            state: .toBuy,
+            categoryId: nil,
+            listId: .fromUuid(list.uuid)
+        )
+    }
 }
